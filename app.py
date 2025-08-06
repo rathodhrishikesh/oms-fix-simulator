cc829b80b14745f2e9de3744c2126f615e69e92c
rathodhrishikesh <rathodhrishikesh@gmail.com>
2025-08-05 18:24:13 -0700

    Fix cols Home Page

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import streamlit as st
 from oms_simulation import oms_ui
 
+st.set_page_config(page_title="TradePilot OMS & FIX Protocol Simulator", layout="wide")
+
 # Top layout: Creator info on right
 col1, col2 = st.columns([6, 4])
 with col1:
-    pass  # Empty space for title alignment
+    st.title("📊 TradePilot: OMS & FIX Protocol Simulator")
+
 with col2:
     st.markdown("""
     <div style='text-align: right; font-size: 14px;'>
@@ -12,9 +15,5 @@ with col2:
         <b>Portfolio:</b> <a href="https://hr-msba-portfolio.netlify.app/" target="_blank">hr-msba-portfolio.netlify.app</a>
     </div>
     """, unsafe_allow_html=True)
-
-st.set_page_config(page_title="TradePilot OMS & FIX Protocol Simulator", layout="wide")
-
-st.title("📊 TradePilot: OMS & FIX Protocol Simulator")
 
 oms_ui()
